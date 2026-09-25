6596e72b22b0ee993f0a567d0ee41bddf4b392c4
Bryan Jose Moran Palacios <gtrmovistar15@sales.group>
2026-09-25 09:31:43 -0500

    Corregir conexión con Supabase

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 //   - SUPABASE_URL     -> "Project URL"
 //   - SUPABASE_ANON_KEY -> "anon public" key (NO uses la "service_role")
 const SUPABASE_CONFIG = {
-  url: "https://cnqxdqcrwibvluljenje.supabase.co/rest/v1/",
+  url: "https://cnqxdqcrwibvluljenje.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNucXhkcWNyd2lidmx1bGplbmplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzMjUyODksImV4cCI6MjEwNTkwMTI4OX0.FvIREP1bSZDhhjwK4pdG6iTs01ellBkpCtfreD71njM",
 };
